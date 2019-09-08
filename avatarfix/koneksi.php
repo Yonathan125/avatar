293e88920a321bd78fd203f08984afe7d20b4a6c
kalious09 <kalious09@gmail.com>
2019-09-08 23:28:17 +0700

    tugas ibd

--- avatarfix/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host = "localhost"; // Nama hostnya
-$username = "root"; // Username
-$password = ""; // Password (Isi jika menggunakan password)
-$database = "dataanak"; // Nama databasenya
-$connect = mysqli_connect($host, $username, $password, $database); // Koneksi ke MySQL
-?>
